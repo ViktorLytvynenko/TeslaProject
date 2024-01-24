888b6605315f5263fc45bb2a4c353bfcbc0af6ac
KarinaIvaschenko <karinka2323@gmail.com>
2024-01-24 14:45:56 +0200

    edit home

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,40 +1,49 @@
-import {FC} from "react";
+import { FC } from "react";
 import {
     View,
     StyleSheet,
     Text,
     Image,
-    TouchableWithoutFeedback
+    TouchableWithoutFeedback,
 } from "react-native";
-import {LinearGradient} from "expo-linear-gradient";
+import { LinearGradient } from "expo-linear-gradient";
 
-const Home: FC<any> = ({navigation}) => {
+const Home: FC<any> = ({ navigation }) => {
     return (
         <View style={s.container}>
             <LinearGradient
                 colors={["#292C31", "#000", "#000", "#292929"]}
                 locations={[0, 0.3009, 0.5735, 1]}
-                start={{x: 0, y: 0}}
-                end={{x: 1, y: 1}}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
                 style={s.container}
             >
-                <View style={s.settingsContainer}>
-                    <TouchableWithoutFeedback style={s.settingsContainerC1}>
-                        <Image source={require("../assets/button.png")} style={s.settingsContainerC1Img}/>
+                <View style={s.wrapper}>
+                    <TouchableWithoutFeedback style={s.settings}>
+                        <Image
+                            source={require("../assets/button.png")}
+                            style={s.settingsImg}
+                        />
                     </TouchableWithoutFeedback>
-                    <View style={s.settingsContainerC2}>
-                        <Image source={require("../assets/carDark.png")} style={s.settingsContainerC2Img}/>
+                    <View style={s.carDark}>
+                        <Image
+                            source={require("../assets/carDark.png")}
+                            style={s.carDarkImg}
+                        />
                     </View>
                     <LinearGradient
                         colors={["#17171C", "#18191B"]}
                         locations={[0.0368, 0.4642]}
-                        style={s.settingsContainerC3}>
-                        <View style={s.settingsContainerC3Inner}>
-                            <Text style={s.settingsContainerC3InnerText}>Unlock</Text>
-                            <TouchableWithoutFeedback style={s.settingsContainerC3InnerBox}>
-                                <Image source={require("../assets/buttonLock.png")} style={s.settingsContainerC3InnerBoxImg}/>
+                        style={s.unlockGradient}
+                    >
+                        <View style={s.unlockContainer}>
+                            <Text style={s.unlockText}>Unlock</Text>
+                            <TouchableWithoutFeedback style={s.unlockBtn}>
+                                <Image
+                                    source={require("../assets/buttonLock.png")}
+                                    style={s.unlockImg}
+                                />
                             </TouchableWithoutFeedback>
-
                         </View>
                     </LinearGradient>
                 </View>
@@ -47,68 +56,67 @@ const s = StyleSheet.create({
     container: {
         width: "100%",
         height: "100%",
-        marginTop: 25 // test
+        marginTop: 25, // test
     },
-    settingsContainer: {
+    wrapper: {
         paddingTop: 25, // test
         width: "100%",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
     },
-    settingsContainerC1: {
-        width: 72,
-        height: 72,
-    },
-    settingsContainerC1Img: {
+    // settings: {
+    //     width: 72,
+    //     height: 72,
+    // },
+    settingsImg: {
         alignSelf: "flex-end",
         width: 72,
         height: 72,
     },
-    settingsContainerC2: {
+    carDark: {
         width: 245,
         height: 163,
-        marginTop: 132
+        marginTop: 132,
     },
-    settingsContainerC2Img: {
+    carDarkImg: {
         width: "100%",
         height: "100%",
     },
-    settingsContainerC3: {
+    unlockGradient: {
         display: "flex",
         flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
         borderRadius: 40,
-        marginTop: 217
+        marginTop: 217,
     },
-    settingsContainerC3Inner: {
+    unlockContainer: {
         display: "flex",
         flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
         columnGap: 20,
-        paddingHorizontal: 20,
     },
-    settingsContainerC3InnerText: {
+    unlockText: {
         textAlign: "center",
         color: "white",
         fontWeight: "600",
         fontSize: 17,
         lineHeight: 22,
-        letterSpacing: -0.408
-},
-    settingsContainerC3InnerBox: {
-        width: 72,
-        height: 72,
+        letterSpacing: -0.408,
+        paddingHorizontal: 20,
     },
-    settingsContainerC3InnerBoxImg: {
+    // unlockBtn: {
+    //     width: 72,
+    //     height: 72,
+    // },
+    unlockImg: {
         marginTop: 7,
         width: 72,
         height: 72,
-    }
-
+    },
 });
 
 export default Home;
