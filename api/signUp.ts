978b795bb3a4407495c238ca75a33f1a76d2fef6
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-03-11 10:41:43 +0200

    changed backend

--- api/signUp.ts
+++ api/signUp.ts
@@ -2,7 +2,7 @@ import {instance} from "./instance";
 
 export const sendCandidateToSignUp = async (formData) => {
     try {
-        const {data} = await instance.post("user/create", formData);
+        const {data} = await instance.post("customers", formData);
         return data;
     } catch (error) {
         throw new Error(error);
