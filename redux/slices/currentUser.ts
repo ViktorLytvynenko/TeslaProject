bb4ea318f22427bf70080885983423e93ac139e8
KarinaIvaschenko <karinka2323@gmail.com>
2024-03-12 23:35:55 +0200

    redux

--- redux/slices/currentUser.ts
+++ redux/slices/currentUser.ts
@@ -1,50 +1,65 @@
-import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {getUserInfo} from "../../api/getUserInfo";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { getUserInfo } from "../../api/getUserInfo";
 
 export interface IStateCurrentUser {
-    email: string,
-    telephone: string,
-    firstName: string,
-    lastName: string,
-    login: string,
-    password: string
-    isLoading: boolean
+    email: string;
+    telephone: string;
+    firstName: string;
+    lastName: string;
+    login: string;
+    password: string;
+    isLoading: boolean;
 }
 
 const initialState: IStateCurrentUser = {
-    email: '',
-    telephone: '',
-    firstName: '',
-    lastName: '',
-    login: '',
-    password: '',
-    isLoading: false
+    email: "",
+    telephone: "",
+    firstName: "",
+    lastName: "",
+    login: "",
+    password: "",
+    isLoading: false,
 };
 
-export const getCurrentUser = createAsyncThunk("users/getCurrentUser", async () => {
-    return await getUserInfo();
-})
+export const getCurrentUser = createAsyncThunk(
+    "users/getCurrentUser",
+    async () => {
+        return await getUserInfo();
+    }
+);
 
 const currentUserSlice = createSlice({
     name: "currentUser",
     initialState,
     reducers: {
+        resetData: (state) => {
+            state.email = "";
+            state.telephone = "";
+            state.firstName = "";
+            state.lastName = "";
+            state.login = "";
+            state.password = "";
+            state.isLoading = false;
+        },
     },
     extraReducers: (builder) => {
         builder.addCase(getCurrentUser.pending, (state: IStateCurrentUser) => {
-            state.isLoading = true
-        })
-        builder.addCase(getCurrentUser.fulfilled, (state: IStateCurrentUser, action: PayloadAction<any>) => {
-            state.isLoading = false;
-            state.email = action.payload.email;
-            state.telephone = action.payload.telephone;
-            state.firstName = action.payload.firstName;
-            state.lastName = action.payload.lastName;
-            state.login = action.payload.login;
-            state.password = action.payload.password;
-        })
-    }
+            state.isLoading = true;
+        });
+        builder.addCase(
+            getCurrentUser.fulfilled,
+            (state: IStateCurrentUser, action: PayloadAction<any>) => {
+                state.isLoading = false;
+                state.email = action.payload.email;
+                state.telephone = action.payload.telephone;
+                state.firstName = action.payload.firstName;
+                state.lastName = action.payload.lastName;
+                state.login = action.payload.login;
+                state.password = action.payload.password;
+            }
+        );
+    },
 });
 
-export const {getUser} = currentUserSlice.actions;
+export const { resetData } = currentUserSlice.actions;
 export default currentUserSlice.reducer;
